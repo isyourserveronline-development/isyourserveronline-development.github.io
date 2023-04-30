d8728750b6ec9b3f98a0f10114c07edd25a6b6a0
Carpo <103697272+Carpodi@users.noreply.github.com>
2023-04-30 18:22:18 -0400

    Website upload.

---
+++ ServerList/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	const serverList = $('#server-list');
+
+	// Lista de servidores de Minecraft
+	const servers = [
+		{ name: 'Hypixel Network', domain: 'hypixel.net' },
+		{ name: 'Kaory Network', domain: 'mc.kaory.xyz' }
+	];
+
+	// Función para obtener información del servidor
+	const getServerInfo = (server) => {
+		const url = `https://api.mcsrvstat.us/2/${server.domain}`;
+
+		$.get(url, function(response) {
+			let players = response.players.online;
+			let maxPlayers = response.players.max;
+			let connection = response.online ? 'En línea' : 'Fuera de línea';
+
+			serverList.append(`
+				<tr>
+					<td>${server.name}</td>
+					<td>${server.domain}</td>
+					<td>${players} / ${maxPlayers}</td>
+					<td class="connection-${response.online ? 'online' : 'offline'}">${connection}</td>
+				</tr>
+			`);
+		}).fail(function() {
+			serverList.append(`
+				<tr>
+					<td>${server.name}</td>
+					<td>${server.domain}</td>
+					<td colspan="2" class="connection-offline">Fuera de línea</td>
+				</tr>
+			`);
+		});
+	};
+
+	// Obtener información de cada servidor y mostrarla en la tabla
+	servers.forEach(function(server) {
+		getServerInfo(server);
+	});
+});
---
+++ scripts.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input[type="text"]');
+const status = document.querySelector('#status');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const serverIP = input.value;
+
+	fetch(`https://api.mcsrvstat.us/2/${serverIP}`)
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.online) {
+				const players = `${data.players.online}/${data.players.max}`;
+				const motd = data.motd.clean[0];
+				status.innerHTML = `
+					<div class="server-status">
+						<p>Server Status <span class="online">Online</span></p>
+						<p>Players Online: ${players}</p>
+					</div>
+					<div class="server-motd">
+						<p>MOTD: ${motd}</p>
+					</div>
+				`;
+			} else {
+				status.innerHTML = `
+					<div class="server-status">
+						<p>Server Status <span class="offline">Offline</span></p>
+					</div>
+				`;
+			}
+		})
+		.catch((error) => {
+			status.textContent = 'Unknown Error..';
+			console.error(error);
+		});
+});

7ab129e9f89ec45ea199b8c3651e3142516b9120
HappyRogelio7 <58347721+HappyRogelio7@users.noreply.github.com>
2023-04-30 22:21:38 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,15 @@ const form = document.querySelector('form');
 const input = document.querySelector('input[type="text"]');
 const status = document.querySelector('#status');
 
+// Recupera el valor del parámetro "ip" de la URL
+const urlParams = new URLSearchParams(window.location.search);
+const ip = urlParams.get('ip');
+
+// Si se proporcionó una dirección IP en la URL, insertarla automáticamente en el campo de búsqueda,
+if (ip) {
+   document.getElementById('server-ip').value = ip;
+}
+
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
 

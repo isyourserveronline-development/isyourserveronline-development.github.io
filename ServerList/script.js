addf6f02c0dfb552a632cf2add1f0932de141c69
Happy7 <58347721+Happyuky7@users.noreply.github.com>
2023-12-14 03:30:02 -0300

    Update script.js

--- ServerList/script.js
+++ ServerList/script.js
@@ -10,8 +10,6 @@ $(document).ready(function() {
 		{ name: 'RyzenNet', domain: 'mc.ryzennet.xyz'},
 		{ name: 'AtlantisMC', domain: 'atlantismc.eu'},
 		{ name: 'Mazer Network', domain: 'mazerclub.com'},
-		{ name: 'FortyCraft Network', domain: 'mc.fortycraft.xyz'},
-		{ name: 'Watones Network', domain: 'mc.watones.xyz'}
 	];
 
 	// Función para obtener información del servidor
